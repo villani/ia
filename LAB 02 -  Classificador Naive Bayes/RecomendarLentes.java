20a6ef0919596ce46abd3973e07918ecb7159521
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2020-10-31 10:24:21 -0300

    Exemplos planejados para a Semana 10

---
+++ LAB 02 -  Classificador Naive Bayes/RecomendarLentes.java
@@ -0,0 +1,43 @@
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+class RecomendarLentes {
+
+    public static void main(String[] args) throws Exception {
+        
+        // Definir a origem dos dados.
+        DataSource arff = new DataSource("contact-lenses.arff");
+
+        // Definir o objeto que representa o conjunto de dados.
+        Instances lenses = arff.getDataSet(4); // - atributo com índice 4 (atributo classe)
+
+        // Definir o objeto que representa o paciente que receberá a recomendação.
+        Instance paciente = new DenseInstance(5);   // - 5 atributos.
+        paciente.setDataset(lenses);                // - como serão os 5 atributos;
+        paciente.setValue(0, "presbyopic");              // 0 - age
+        paciente.setValue(1, "myope");              // 1 - spectacle-prescrip
+        paciente.setValue(2, "no");                 // 2 - astigmatism
+        paciente.setValue(3, "reduced");             // 3 - tear-prod-rate
+        
+        // Definir o objeto que fará a recomendação (o classificador).
+        NaiveBayes bayes = new NaiveBayes();
+
+        // Treinar o classificador.
+        bayes.buildClassifier(lenses);
+
+        // Fazer a recomendação.
+        double recomendacao = bayes.classifyInstance(paciente);
+        paciente.setValue(4, recomendacao);
+
+        // Exibir a recomendação feita.
+        System.out.println("\n\nRecomendação: " + recomendacao);
+        System.out.println("Rótulo: " + paciente.stringValue(4));
+
+        // Exibindo o modelo construído pelo classificador
+        System.out.println(bayes);
+
+    }
+}
